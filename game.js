da5d5fbfd62bab692a92128944ae5c133d08f644
TimKis <artemkis4321@gmail.com>
2021-06-29 15:11:59 -0700

    add menu in the game

--- game.js
+++ game.js
@@ -7,6 +7,11 @@
 
     create(){
       this.add.text(150, 250, 'Let the Game begin', {fontSize: '15px', fill: '#000000'});
+
+      this.input.on('pointerup', () => {
+        this.scene.stop('StartScene');
+        this.scene.start('GameScene');
+      });
     }
   };
   
